195ded9c4e68eec156aa472b17fb73bf6771a5ce
Saxenaispriya <saxena23priya@gmail.com>
2026-08-28 12:06:13 +0530

    Add Key Vault integration for order processor function

--- RestaurantOrderProcessorFunction/Program.cs
+++ RestaurantOrderProcessorFunction/Program.cs
@@ -1,9 +1,21 @@
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+var keyVaultUrl = new Uri("https://restaurant-keyvault-joey.vault.azure.net/");
+
+var secretClient = new SecretClient(
+    keyVaultUrl,
+    new DefaultAzureCredential());
+
+var secret = secretClient.GetSecret("ServiceBusConnection");
+
+Console.WriteLine($"Key Vault secret loaded successfully: {secret.Value.Name}");
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
